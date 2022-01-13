e1674b84ffa25d139a9e60fd51160978b9b51645
beliyzy <belyaevds668@gmail.com>
2022-01-13 19:41:33 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">/div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;700&display=swap');
+
+$main-font: 'Rubik', sans-serif;
+$footer-font: 'IBM Plex Sans', sans-serif;
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: $main-font;
+}
+
+.container {
+  width: 85%;
+  margin: 0 auto;
+}
+</style>
